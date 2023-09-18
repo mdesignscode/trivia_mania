ab892dacf9c7fff4076a6389e3d630811dec18f4
Mdesignscode <marlonbaatjes62@gmail.com>
2023-09-18 19:14:02 +0200

    🖼📊🤸‍♂0️⃣ update UI for when user has no stats 🖼📊🤸‍♂0️⃣

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,5 +1,6 @@
 import storage from "@/models/index";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function UserProgress({
   params: { id },
@@ -34,70 +35,82 @@ export default function UserProgress({
         <h1 className="text-3xl">{user.username}</h1>
       </div>
 
-      {/* Hero */}
-      <div>
-        <h2>This is your progress</h2>
-        {topTenPosition > 0 && (
-          <h2>
-            Congratulations you are number {topTenPosition} in the Leader board
-          </h2>
-        )}
+      {Object.keys(userStats).length > 0 ? (
+        <>
+          {/* Hero */}
+          <div>
+            <h2>This is your progress</h2>
+            {topTenPosition > 0 && (
+              <h2>
+                Congratulations you are number {topTenPosition} in the Leader
+                board
+              </h2>
+            )}
 
-        <h3>Total questions answered: {userStats.total.answered}</h3>
-        <h3>Total correct answers: {userStats.total.correctAnswered}</h3>
-      </div>
+            <h3>Total questions answered: {userStats.total.answered}</h3>
+            <h3>Total correct answers: {userStats.total.correctAnswered}</h3>
+          </div>
 
-      {/* Body */}
-      <div className="flex flex-col md:flex-row gap-3">
-        {sortedKeys.map((stat) => {
-          if (["easy", "hard", "medium"].includes(stat)) {
-            const capitalized = stat.split("");
-            capitalized[0] = capitalized[0].toUpperCase();
-            const capitalizedDifficulty = capitalized.join("");
-            // display cards based on difficulty
-            return (
-              <div
-                className="flex flex-col md:w-1/3 gap-3 bg-white p-4 rounded-lg shadow-md"
-                key={stat}
-              >
-                <div className="flex justify-between border-b-2 border-gray-800">
-                  <h3 className="text-2xl">{capitalizedDifficulty}</h3>
-                  <p>
-                    {userStats[stat].correctAnswered}/{userStats[stat].answered}
-                  </p>
-                </div>
+          {/* Body */}
+          <div className="flex flex-col md:flex-row gap-3">
+            {sortedKeys.map((stat) => {
+              if (["easy", "hard", "medium"].includes(stat)) {
+                const capitalized = stat.split("");
+                capitalized[0] = capitalized[0].toUpperCase();
+                const capitalizedDifficulty = capitalized.join("");
+                // display cards based on difficulty
+                return (
+                  <div
+                    className="flex flex-col md:w-1/3 gap-3 bg-white p-4 rounded-lg shadow-md"
+                    key={stat}
+                  >
+                    <div className="flex justify-between border-b-2 border-gray-800">
+                      <h3 className="text-2xl">{capitalizedDifficulty}</h3>
+                      <p>
+                        {userStats[stat].correctAnswered}/
+                        {userStats[stat].answered}
+                      </p>
+                    </div>
 
-                <div className="flex flex-col gap-3">
-                  {/* display categories by difficulty */}
-                  {sortedKeys.map((categoryStat) => {
-                    if (
-                      !["total", "easy", "hard", "medium"].includes(
-                        categoryStat
-                      ) &&
-                      userStats[categoryStat][stat]
-                    ) {
-                      return (
-                        <div key={`${stat}_${categoryStat}`}>
-                          <h4>{categoryStat}</h4>
-                          <div className="ml-3">
-                            <p>
-                              total: {userStats[categoryStat][stat].answered}
-                            </p>
-                            <p>
-                              correct:{" "}
-                              {userStats[categoryStat][stat].correctAnswered}
-                            </p>
-                          </div>
-                        </div>
-                      );
-                    }
-                  })}
-                </div>
-              </div>
-            );
-          }
-        })}
-      </div>
+                    <div className="flex flex-col gap-3">
+                      {/* display categories by difficulty */}
+                      {sortedKeys.map((categoryStat) => {
+                        if (
+                          !["total", "easy", "hard", "medium"].includes(
+                            categoryStat
+                          ) &&
+                          userStats[categoryStat][stat]
+                        ) {
+                          return (
+                            <div key={`${stat}_${categoryStat}`}>
+                              <h4>{categoryStat}</h4>
+                              <div className="ml-3">
+                                <p>
+                                  total:{" "}
+                                  {userStats[categoryStat][stat].answered}
+                                </p>
+                                <p>
+                                  correct:{" "}
+                                  {
+                                    userStats[categoryStat][stat]
+                                      .correctAnswered
+                                  }
+                                </p>
+                              </div>
+                            </div>
+                          );
+                        }
+                      })}
+                    </div>
+                  </div>
+                );
+              }
+            })}
+          </div>
+        </>
+      ) : (
+        <h2>You have not answered any questions yet. <Link className="text-gray-600 hover:text-gray-800 hover:underline decoration-solid" href="/">Click here to play</Link>.</h2>
+      )}
     </div>
   );
 }

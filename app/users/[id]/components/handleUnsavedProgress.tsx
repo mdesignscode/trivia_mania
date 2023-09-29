ce658cef4842f09243bce3757c7d133bbd7eeaed
Mdesignscode <marlonbaatjes62@gmail.com>
2023-09-29 23:56:51 +0200

    🏗️🤸‍♀️📊💾 handle unsaved progress 🏗️🤸‍♀️📊💾

--- app/users/[id]/components/handleUnsavedProgress.tsx
+++ app/users/[id]/components/handleUnsavedProgress.tsx
@@ -1,31 +1,32 @@
 "use client";
 import { Button } from "@/components/styledComponents";
-import { IUser } from "@/models/interfaces";
 import { useUser } from "@clerk/nextjs";
+import { GlobalContext } from "app/store";
 import axios from "axios";
-import { userAgent } from "next/server";
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useContext, useState } from "react";
 
 export default function HandleUnsavedProgress() {
   const { user } = useUser();
   const [hasUnsavedProgress, setHasUnsavedProgress] = useState(false);
+  const { storageIsAvailable } = useContext(GlobalContext);
 
   async function saveProgress() {
-    const hasUnsavedData = localStorage.getItem("unsavedData");
-    if (hasUnsavedData && user) {
-      setHasUnsavedProgress(true);
-      const progress = JSON.parse(hasUnsavedData);
-      const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-      const url = `${baseUrl}/users/updateStats`;
-      const id = user.id;
-      localStorage.setItem("unsavedData", "{}");
+    if (storageIsAvailable) {
+      const hasUnsavedData = localStorage.getItem("unsavedData");
+      if (hasUnsavedData && user) {
+        setHasUnsavedProgress(true);
+        const progress = JSON.parse(hasUnsavedData);
+        const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+        const url = `${baseUrl}/users/updateStats`;
+        const id = user.id;
+        localStorage.setItem("unsavedData", "{}");
 
-      try {
-        const { data } = await axios.post(url, { stats: progress, id });
-        return data;
-      } catch (error) {
-        return error;
+        try {
+          const { data } = await axios.post(url, { stats: progress, id });
+          return data;
+        } catch (error) {
+          return error;
+        }
       }
     }
   }

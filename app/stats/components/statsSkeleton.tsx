88e132bb5b561aa4c5a7b4ca9d800f13b0d7d62c
Mdesignscode <marlonbaatjes@gmail.com>
2024-02-25 19:03:42 +0200

    🪄

--- app/stats/components/statsSkeleton.tsx
+++
@@ -1,7 +0,0 @@
-export default function StatsSkeleton() {
-  return (
-    <div className="text-xl text-gray-800 w-11/12 col gap-4 mx-auto py-4 overflow-y-auto">
-      <div className="flex items-center gap-5"></div>
-    </div>
-  )
-}

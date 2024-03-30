5bb925d8298e0e22decd7adb07a11a355d2f8362
Mdesignscode <marlonbaatjes@gmail.com>
2024-03-30 20:31:55 +0200

    🪄

--- prisma/migrations/20240223171026_no_avatar/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `avatar` on the `UserStats` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "UserStats" DROP COLUMN "avatar";
